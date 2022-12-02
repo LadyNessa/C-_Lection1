40e5538a3d46965fb555e528920a61841154e15e
Nessa <la-nessa@mail.ru>
2022-12-02 22:18:13 +0300

    Ex005

---
+++ Ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username == "Мария")
+{
+    Console.WriteLine("Ура, это же Мария!");
+}
+else 
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
